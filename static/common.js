0b2feef73a51dee4055eb29da186ab66d6d2e701
quanxin <3059210183@qq.com>
2019-03-26 09:48:55 +0800

    h5旋转图片完成

--- static/common.js
+++ static/common.js
@@ -7,11 +7,45 @@
  * 别在这里写东西!!
  *
  * */
-
-function getMid() {
-    var userInfoData = null;
-    if (typeof (H5CallNativieUtils) != "undefined") {
-        userInfoData = JSON.parse(H5CallNativieUtils.getUserInfo());
+//  获取用户信息
+function getUserInfo() {
+    var userInfo = null;
+    if (typeof  (H5CallNativieUtils) != 'undefined') {
+        userInfo = JSON.parse(H5CallNativieUtils.getUserInfo());
     }
-    return userInfoData.mid;
+    return userInfo;
 }
+//  mid
+function getMid() {
+    return getUserInfo().mid;
+}
+//  app版本号
+function getAppVersion() {
+    return getUserInfo().appVersion;
+}
+
+//  手机操作系统
+function getPlatform() {
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
+    if (isAndroid) {
+        return 'isAndroid';
+    }
+}
+//  判断版本
+var defaultVersion = '3.0.2'.split('.');
+function checkVersion(val) {
+    var _temp = val.split('.');
+    if (_temp.length !== 3) {
+        return true
+    }
+    for (var i = 0; i < defaultVersion.length; i++) {
+        if (defaultVersion[i] > _temp[i]) {
+            return false;
+        } else if (defaultVersion[i] < _temp[i]) {
+            return true;
+        }
+    }
+    return true;
+}
+
